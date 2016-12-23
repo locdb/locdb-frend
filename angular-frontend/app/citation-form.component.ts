124dc077c26e5f6f3b160f547cc9d3a1f83378b8
Lukas Galke <git@lpag.de>
2016-12-23 12:20:38 +0100

    not quite the fix for REFTYPES

--- angular-frontend/app/citation-form.component.ts
+++ angular-frontend/app/citation-form.component.ts
@@ -6,25 +6,25 @@ import { Citation } from './citation';
   selector: 'citation-form',
   templateUrl: 'citation-form.component.html'
 })
-const REFTYPES = ['Article', 'Journal', 'Book', 'Other'];
 export class CitationFormComponent {
+  REFTYPES = ['Article', 'Journal', 'Book', 'Other'];
   @Input()
   references: Citation[] = [
     new Citation(42,
                  'LOCDB',
-                 REFTYPES[1],
+                 this.REFTYPES[1],
                  ['Abramson, L.Y.', 'Seligman, M.E.P.', 'Teasdale, J.D.'],
                  'Learned helplessness in hu-mans: Critique and reformula',
                  1978, 'Journal of Abnormal Psychology', null, 87, '49-74'),
 
     new Citation(43,
                  'DOAJ',
-                 REFTYPES[1],
+                 this.REFTYPES[1],
                  ['Abramson, L.Y.', 'Seligman, M.E.P.', 'Teasdale, J.D.'],
                  'Learned healthiness in humans: Critique and reformula',
                  1978, 'Journal of Normal Psychology', null, 87, '46-78'),
 
-                 new Citation(44, 'DOAJ', REFTYPES[0], ['Adorno, T.W.'],
+                 new Citation(44, 'DOAJ', this.REFTYPES[0], ['Adorno, T.W.'],
                               'Studien zum autoritären Charakter', 1973,
                               'Suhrkamp (Frankfurt)')
 
