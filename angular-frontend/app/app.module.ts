176c7739f3eb4e1791f2f90f044ac1c325d83f22
Lukas Galke <git@lpag.de>
2017-01-16 16:44:37 +0100

    fixes

--- angular-frontend/app/app.module.ts
+++ angular-frontend/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'
-import { RouterModule } from '@angular/router'
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 // import { ImageUploadModule } from 'ng2-imageupload';
 
