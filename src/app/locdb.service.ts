92e48a26164b91f4c2d49811e60da484e449284f
Lukas Galke <git@lpag.de>
2017-03-21 10:09:55 +0100

    service

--- src/app/locdb.service.ts
+++ src/app/locdb.service.ts
@@ -17,16 +17,48 @@ import { REFERENCES, EXTERNAL_REFERENCES } from './mock-references';
 
 @Injectable()
 export class LocdbService {
-  private locdbUrl = 'http://velsen.informatik.uni-kiel/';
+
+  // we could read this from some config file
+  private locdbUrl                      = 'http://velsen.informatik.uni-kiel:80/';
+  private locdbTodoEndpoint             = this.locdbUrl + 'getToDo'
+  private locdbSaveScan                 = this.locdbUrl + 'saveScan'
+  private bibliographicResourceEndpoint = this.locdbUrl + 'bibliographicResources'
+  private bibliographicEntriesEndpoint  = this.locdbUrl + 'getToDoBibliographicEntries'
 
   constructor(private http: Http) { }
 
-  getTodos(): Promise<Todo[]> {
 
-    return Promise.resolve(TODOS);
-
+  // Generic helpers for data extraction and error handling
+  
+  private extractData(res: Response) {
+    let body = res.json();
+    return body.data || { };
   }
 
+  private handleError (error: Response | any) {
+    // In a real world app, you might use a remote logging infrastructure
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
+
+  // acquire todo items and scans
+  getTodos(): Observable<Todo[]> {
+    return this.http.get(this.locdbTodoEndpoint).map(this.extractData).catch(this.handleError);
+  }
+
+  // create scans
+  
+  // related to bibligraphic entries
+
+  // related to bibliographic resources
   getBibliographicResources(forTodo: Todo) {
     console.log('Querying appropriate BResources for', forTodo);
 
@@ -38,5 +70,6 @@ export class LocdbService {
 
     return Promise.resolve(REFERENCES);
   }
+
 
 }
