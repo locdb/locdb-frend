196814e4908c7dfc755fdda471c37ac8bf9e0afa
Lukas Galke <git@lpag.de>
2017-10-11 14:17:24 +0200

    fix deploy script

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,5 +1,5 @@
 BASE_HREF=https://locdb.bib.uni-mannheim.de/extrapolate-dev/
 echo "Building for $BASE_HREF"
-ng build --base-href=$BASE_HREF || echo "Build failed, abort." && exit -1
+ng build --base-href=$BASE_HREF
 echo "Copying files to remote web server"
 scp dist/* locdb:/home/lga/www/extrapolate-dev
