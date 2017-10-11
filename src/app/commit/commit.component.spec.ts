9e87e3e11f26c338ef89e63e24bf042eba6efd30
Lukas Galke <git@lpag.de>
2017-10-11 17:13:08 +0200

    once again the test bed

--- src/app/commit/commit.component.spec.ts
+++ src/app/commit/commit.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LocdbService } from './locdb.service';
+import { LocdbService } from '../locdb.service';
 import { CommitComponent } from './commit.component';
 
 describe('CommitComponent', () => {
