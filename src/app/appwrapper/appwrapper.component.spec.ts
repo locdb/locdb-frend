8438b628b5b4d846f51ce06497cc3f1e2de42e15
Fabian Steputat <stu113182@mail.uni-kiel.de>
2017-11-21 13:52:32 +0100

    added routings, navbar

---
+++ src/app/appwrapper/appwrapper.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AppwrapperComponent } from './appwrapper.component';
+
+describe('AppwrapperComponent', () => {
+  let component: AppwrapperComponent;
+  let fixture: ComponentFixture<AppwrapperComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AppwrapperComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppwrapperComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/appwrapper/appwrapper.component.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ToDo, ToDoParts, ToDoScans, BibliographicEntry, BibliographicResource } from '../locdb';
+import { LocdbService } from '../locdb.service';
+import { FeedService } from '../feed.service';
+//import { environment } from 'environments/environment';
+
+
+  @Component({
+      selector: 'app-wrap',
+      templateUrl: './appwrapper.component.html',
+      providers: [ LocdbService, FeedService ]
+  })
+
+  /** Main App Component for whole LOCDB Frontend */
+  export class AppwrapperComponent implements OnInit {
+      title = 'LOC-DB Frontend';
+      source: BibliographicResource = null;
+      todo: ToDoScans | ToDo = null;
+      resourceTrack: BibliographicResource[] | ToDo[];
+      entry: BibliographicEntry = null;
+      target: BibliographicResource = null;
+      constructor (private locdbService: LocdbService ) {}
+
+      ngOnInit() {
+          // this.visualState = 0;
+      }
+
+      updateTodo(todo: ToDoScans | BibliographicResource) {
+          // this.visualState = 0;
+          this.todo = todo;
+      }
+
+      updateTrack(resources: BibliographicResource[] | ToDo[]){
+          this.resourceTrack = resources;
+      }
+
+      updateSource (br: BibliographicResource) {
+          // this.enableCitation();
+          // this.visualState = 2;
+          console.log('Updating source', br);
+          this.source = br;
+      }
+
+      updateEntry (entry: BibliographicEntry) {
+          // the check on entry causes the infamous ExpressionChangedAfterItWasSet exception
+          // this.showCitation()
+          // if (entry) {
+          //     this.visualState = 1;
+          // } else {
+          //     this.visualState = 0;
+          // }
+          console.log('Updating with new entry', entry);
+          this.entry = entry;
+          // reset resource, since we selected a different entry
+          this.target = null;
+      }
+
+      updateTarget (br: BibliographicResource) {
+          // this.enableCitation();
+          // this.visualState = 2;
+          console.log('Updating target', br);
+          this.target = br;
+      }
+
+
+      /* Are the functions below used? */
+
+      // roundUp(num, precision) {
+      //     return Math.ceil(num * precision) / precision;
+      // }
+
+      // pathStart() {
+      //     console.log('pathStart');
+      // }
+
+      // pathSelectResource() {
+      //     console.log('pathSelectResource');
+      // }
+
+      // pathEditAndSubmit() {
+      //     console.log('pathEditAndSubmit');
+      // }
+
+
+}
