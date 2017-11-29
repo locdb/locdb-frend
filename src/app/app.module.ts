16e3400a206e614abbd748d20cf4f8ab12ae00aa
Lukas Galke <git@lpag.de>
2017-11-29 16:13:21 +0100

    no safe html pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -53,7 +53,6 @@ import { ResourceEditableComponent } from './resource-editable/resource-editable
 import { ResourceAccordionGroupComponent } from './resource-accordion-group/resource-accordion-group.component';
 import { AppwrapperComponent } from './appwrapper/appwrapper.component';
 import { FrontpageComponent } from './frontpage/frontpage.component';
-import { SafeHtmlPipe } from './safe-html.pipe';
 
 const appRoutes: Routes = [
   { path: 'disambiguate', component: AppwrapperComponent },
