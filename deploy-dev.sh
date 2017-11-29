dace066e0672a07d4db242027ea7d0300037d087
Lukas Galke <git@lpag.de>
2017-11-29 13:59:04 +0100

    volatile deploy prod script

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,6 +1,4 @@
-DEPLOY_URL=https://locdb.bib.uni-mannheim.de/extrapolate-dev/
-echo "Building for $DEPLOY_URL"
-# ng build --deploy-url "$DEPLOY_URL" --base-href /extrapolate-dev/
+echo "Building for Development"
 ng build --base-href /extrapolate-dev/
 cat dist/index.html
 echo "Copying files to remote web server"
