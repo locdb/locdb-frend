10d9cc3759e54bcae136ba7a22fadbbc7e59e5f0
Lukas Galke <git@lpag.de>
2017-11-29 13:11:59 +0100

    another minor fix......

--- src/app/appwrapper/appwrapper.component.spec.ts
+++ src/app/appwrapper/appwrapper.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import AppwrapperComponent from './appwrapper.component'
+import { AppwrapperComponent } from './appwrapper.component'
 
 
 describe('AppwrapperComponent', () => {
--- src/app/appwrapper/appwrapper.component.ts
+++ src/app/appwrapper/appwrapper.component.ts
@@ -5,81 +5,79 @@ import { FeedService } from '../feed.service';
 //import { environment } from 'environments/environment';
 
 
-  @Component({
-      selector: 'app-wrap',
-      templateUrl: './appwrapper.component.html',
-      providers: [ LocdbService, FeedService ]
-  })
+@Component({
+  selector: 'app-wrap',
+  templateUrl: './appwrapper.component.html',
+  providers: [ LocdbService, FeedService ]
+})
 
-  /** Main App Component for whole LOCDB Frontend */
-  export class AppwrapperComponent implements OnInit {
-      title = 'LOC-DB Frontend';
-      source: BibliographicResource = null;
-      todo: ToDoScans | ToDo = null;
-      resourceTrack: BibliographicResource[] | ToDo[];
-      entry: BibliographicEntry = null;
-      target: BibliographicResource = null;
-      constructor (private locdbService: LocdbService ) {}
+/** Main App Component for whole LOCDB Frontend */
+export class AppwrapperComponent implements OnInit {
+  title = 'LOC-DB Frontend';
+  source: BibliographicResource = null;
+  todo: ToDoScans | ToDo = null;
+  resourceTrack: BibliographicResource[] | ToDo[];
+  entry: BibliographicEntry = null;
+  target: BibliographicResource = null;
+  constructor (private locdbService: LocdbService ) {}
 
-      ngOnInit() {
-          // this.visualState = 0;
-      }
+  ngOnInit() {
+    // this.visualState = 0;
+  }
 
-      updateTodo(todo: ToDoScans | BibliographicResource) {
-          // this.visualState = 0;
-          this.todo = todo;
-      }
+  updateTodo(todo: ToDoScans | BibliographicResource) {
+    // this.visualState = 0;
+    this.todo = todo;
+  }
 
-      updateTrack(resources: BibliographicResource[] | ToDo[]){
-          this.resourceTrack = resources;
-      }
+  updateTrack(resources: BibliographicResource[] | ToDo[]){
+    this.resourceTrack = resources;
+  }
 
-      updateSource (br: BibliographicResource) {
-          // this.enableCitation();
-          // this.visualState = 2;
-          console.log('Updating source', br);
-          this.source = br;
-      }
+  updateSource (br: BibliographicResource) {
+    // this.enableCitation();
+    // this.visualState = 2;
+    console.log('Updating source', br);
+    this.source = br;
+  }
 
-      updateEntry (entry: BibliographicEntry) {
-          // the check on entry causes the infamous ExpressionChangedAfterItWasSet exception
-          // this.showCitation()
-          // if (entry) {
-          //     this.visualState = 1;
-          // } else {
-          //     this.visualState = 0;
-          // }
-          console.log('Updating with new entry', entry);
-          this.entry = entry;
-          // reset resource, since we selected a different entry
-          this.target = null;
-      }
+  updateEntry (entry: BibliographicEntry) {
+    // the check on entry causes the infamous ExpressionChangedAfterItWasSet exception
+    // this.showCitation()
+    // if (entry) {
+    //     this.visualState = 1;
+    // } else {
+    //     this.visualState = 0;
+    // }
+    console.log('Updating with new entry', entry);
+    this.entry = entry;
+    // reset resource, since we selected a different entry
+    this.target = null;
+  }
 
-      updateTarget (br: BibliographicResource) {
-          // this.enableCitation();
-          // this.visualState = 2;
-          console.log('Updating target', br);
-          this.target = br;
-      }
-
-
-      /* Are the functions below used? */
-
-      // roundUp(num, precision) {
-      //     return Math.ceil(num * precision) / precision;
-      // }
-
-      // pathStart() {
-      //     console.log('pathStart');
-      // }
-
-      // pathSelectResource() {
-      //     console.log('pathSelectResource');
-      // }
-
-      // pathEditAndSubmit() {
-      //     console.log('pathEditAndSubmit');
-      // }
+  updateTarget (br: BibliographicResource) {
+    // this.enableCitation();
+    // this.visualState = 2;
+    console.log('Updating target', br);
+    this.target = br;
+  }
 
 
+  /* Are the functions below used? */
+
+  // roundUp(num, precision) {
+  //     return Math.ceil(num * precision) / precision;
+  // }
+
+  // pathStart() {
+  //     console.log('pathStart');
+  // }
+
+  // pathSelectResource() {
+  //     console.log('pathSelectResource');
+  // }
+
+  // pathEditAndSubmit() {
+  //     console.log('pathEditAndSubmit');
+  // }
 }
