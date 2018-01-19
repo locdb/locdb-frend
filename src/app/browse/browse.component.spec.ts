3c5425ba262fcb5394d0b4143e8b0975f2bda944
Fabian Steputat <stu113182@mail.uni-kiel.de>
2018-01-19 10:18:39 +0100

    fixed travis failures

--- src/app/browse/browse.component.spec.ts
+++ src/app/browse/browse.component.spec.ts
@@ -1,6 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BrowseComponent } from './browse.component';
+import { ResourceAccordionGroupComponent } from '../resource-accordion-group/resource-accordion-group.component';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+import { ResourceEditableComponent } from '../resource-editable/resource-editable.component';
+import { ResourceFormComponent } from '../resource-form/resource-form.component';
+import { ResourceComponent } from '../resource/resource.component';
+import { HttpModule } from '@angular/http';
+import { LocdbService, CredentialsService } from '../locdb.service';
+import { PopoverModule } from 'ngx-popover';
+import { LoggingService } from '../logging.service';
 
 describe('BrowseComponent', () => {
   let component: BrowseComponent;
@@ -8,7 +20,22 @@ describe('BrowseComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BrowseComponent ]
+      imports: [
+        ModalModule.forRoot(),
+        AccordionModule.forRoot(),
+        ReactiveFormsModule,
+        FormsModule,
+        HttpModule,
+        PopoverModule,
+      ],
+      declarations: [
+        BrowseComponent,
+        ResourceComponent,
+        ResourceFormComponent,
+        ResourceEditableComponent,
+        ResourceAccordionGroupComponent
+      ],
+      providers: [ LocdbService, CredentialsService, LoggingService ]
     })
     .compileComponents();
   }));
