f015bbbe0c23b4fd335b1ff58e4c80b3223f86b8
Lukas Galke <git@lpag.de>
2018-10-12 00:30:31 +0200

    Fix #380 container management

--- src/app/resource-form/resource-pair-form.component.ts
+++ src/app/resource-form/resource-pair-form.component.ts
@@ -86,12 +86,13 @@ export class ResourcePairFormComponent implements OnInit {
 
 
   fixPartOf () {
-    /* Link
-     *
+    /* Link resource to its container
+     * The container is either stored as 'alternate' already, or if 'alternate' is null, it will be cleared.
      */
 
     // guard
     if (!this.resource) { return; }
+    if (!this.resource._id) { alert('Please migrate the resource first.'); return; }
 
     // if alternate is not present, the resource should be marked standalone
     let targetId = '';
@@ -105,9 +106,15 @@ export class ResourcePairFormComponent implements OnInit {
     const oldPartOf = this.resource.data.partOf;
 
     // link resource to its container
-    this.resource.data.partOf = targetId;
+
+
+    // ONLY change the partof property ( + add type which is mandatory always )
+    const updates: models.BibliographicResource = { type: this.resource.type, partOf: targetId }
+
+
+
     this.isLoading = true;
-    this.brService.update(this.resource._id, <models.BibliographicResource>this.resource.data).subscribe(
+    this.brService.update(this.resource._id, updates).subscribe(
       (response) => { this.resource = new TypedResourceView(response); this.isLoading = false; },
       (error) => {
         alert('An unexpected error occurred: ' + error.message);
