de1b6471bfcc799f5e91c371fe1118bf751a4944
Lukas Galke <git@lpag.de>
2018-10-19 13:17:54 +0200

    Fix login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { LocdbService } from '../locdb.service';
 import {  Response } from '@angular/http';
 import { models } from '../locdb'
+import { UserService } from '../typescript-angular-client/api/user.service';
 
 @Component({
   selector: 'app-login',
@@ -14,64 +15,71 @@ export class LoginComponent implements OnInit {
   currentUser: models.User = null;
   connecting = false;
 
-  constructor ( private locdbService: LocdbService ) {}
+  constructor ( private userService: UserService ) {}
 
   ngOnInit() {
   }
 
-  validate(user: models.User, msg: Response | any) {
-    if (msg.ok) {
-      console.log('Login succeeded', user, msg.json());
-      this.currentUser = user;
-      this.userChanged.next(true);
-      this.feeds.emit(msg.json().feeds);
-    } else {
-      console.log('Message not ok');
-      alert('Invalid credentials.');
-      this.userChanged.next(false);
-    }
-    this.connecting = false;
+  // validate(user: models.User, msg: Response | any) {
+  //   if (msg.ok) {
+  //     console.log('Login succeeded', user, msg.json());
+  //     this.currentUser = user;
+  //     this.userChanged.next(true);
+  //     this.feeds.emit(msg.json().feeds);
+  //   } else {
+  //     console.log('Message not ok');
+  //     alert('Invalid credentials.');
+  //     this.userChanged.next(false);
+  //   }
+  //   this.connecting = false;
+  // }
+
+  successHandler(user: models.User) {
+    this.currentUser = user;
+    this.userChanged.emit(true);
+    this.feeds.emit(user.feeds || []);
   }
 
-  fail(err: any) {
+  fail(error: any) {
     this.connecting = false;
     this.currentUser = null;
     this.userChanged.next(false);
+    alert(error.message);
   }
 
 
 
-  onSignUp(user: string, pass: string) {
+  onSignUp(username: string, password: string) {
     this.connecting = true;
     // todo fixme
     // this.locdbService.instance(instance).register(user, pass)
     // .then( (message) => this.validate(instance, user, message))
     // .catch((err) => this.fail(err));
-    this.locdbService.register(user, pass).subscribe(
-      (msg) => this.validate(user, msg),
-        (err) => this.fail(err)
+    const user: models.User = { username: username, password: password };
+    this.userService.signup(user).subscribe(
+      (msg) => this.successHandler(user),
+      (err) => this.fail(err)
     );
   }
 
-  onSignIn(user: string, pass: string) {
+  onSignIn(username: string, password: string) {
     this.connecting = true;
     // fancy server interaction
     // this.locdbService.instance(instance).register(user, pass)
     // .map( (message) => this.validate(instance, user, message))
     // .catch((err) => this.fail(err));
-    this.locdbService.login(user, pass).subscribe(
-      (message) => this.validate(user, message),
+    const user: models.User = { username: username, password: password };
+    this.userService.login(user).subscribe(
+      (response) => this.successHandler(response),
       (error) => this.fail(error)
     );
   }
 
   onLogout() {
-    this.locdbService.logout().subscribe(
-      // brute logout
-      (msg) => this.fail(msg),
-      (err) => this.fail(err),
+    this.userService.logout().subscribe(
+      (success) => this.successHandler(null),
+      (error) => this.fail(error),
     );
-      // .catch((err) => this.fail);
   }
 
 }
