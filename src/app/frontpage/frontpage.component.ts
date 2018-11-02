d0322fe59ee6e204055aa29a1b08c3e105f9146e
Lukas Galke <git@lpag.de>
2018-11-02 15:54:18 +0100

    Include solid stats computation and retrieval

--- src/app/frontpage/frontpage.component.ts
+++ src/app/frontpage/frontpage.component.ts
@@ -11,14 +11,33 @@ export class FrontpageComponent implements OnInit {
 
   stats: any;
 
+  statsTriggered = false;
+  isRetrievingStats = false;
+
   constructor(private utilsService: UtilsService) { }
 
   ngOnInit() {
+    // this.retrieveStats();
+  }
+
+  retrieveStats() {
+    this.isRetrievingStats = true;
     console.log('Retrieving stats');
     this.utilsService.stats().subscribe(
-      (stats) => { this.stats = stats; },
-      (error) => { this.stats = error.message; }
+      (stats) => { this.stats = stats; this.isRetrievingStats = false; },
+      (error) => { alert('Error while retrieving stats:' + error.message); this.isRetrievingStats = false; }
     );
   }
 
+  triggerStats() {
+    this.statsTriggered = false;
+
+    if (!confirm('This might take a while...')) { return; }
+
+    this.utilsService.triggerStats().subscribe(
+      (success) => { this.statsTriggered = true; this.retrieveStats(); },
+      (error) => { alert('Triggering stats failed: ' + error.message); }
+    );
+
+  }
 }
